c8db17e2fe55210596c5004c9826d43c0793cff5
Vinícius de Moraes <92277345+viniciusm0raes@users.noreply.github.com>
2023-07-26 20:41:51 -0300

    Update categoria_veiculo.py

--- categoria_veiculo.py
+++ categoria_veiculo.py
@@ -2,7 +2,7 @@ qtd_rodas = eval(input("Quantas rodas possui o veículo? "))
 peso = eval(input("Qual o peso do veículo em Kg? "))
 passageiros = eval(input("Quantos passageiros vão no veículo? "))
 
-if (qtd_rodas < 4):
+if ((qtd_rodas == 2) or (qtd_rodas == 3)):
     print("Categoria A")
 elif (((qtd_rodas == 4) and (peso <= 3500)) and (passageiros <= 8)):
     print("Categoria B")
@@ -12,3 +12,5 @@ elif (((qtd_rodas >= 4) and (peso <= 6000)) or (passageiros >= 8)):
     print("Categoria D")
 elif ((qtd_rodas >= 4) and (peso > 6000)):
     print("Categoria E")
+else:
+    print("A descrição do veículo não é válida.")
