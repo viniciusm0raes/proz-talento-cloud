7cebb5f50fad7f1853347022be043babdec9025b
Vinícius de Moraes <92277345+viniciusm0raes@users.noreply.github.com>
2023-07-29 20:12:24 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,8 @@
-print("Bem vindo à Calculadora.")
+print("-----------------------------------")
 num1 = eval(input("Informe um número: "))
 num2 = eval(input("Informe outro número: "))
 op = input("Informe a operação:\n 1 para Adição\n 2 para Subtração\n 3 para multiplicação\n 4 para divisão:\n ")
+print("-----------------------------------")
 
 if op == '1':
     print("A soma de ", num1, "e", num2, " é: ", num1 + num2)
