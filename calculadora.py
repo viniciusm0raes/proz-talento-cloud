0341d3879c80f611b10e2cb019fc43b86c03d557
Vinícius de Moraes <92277345+viniciusm0raes@users.noreply.github.com>
2023-07-29 22:07:14 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,20 @@
 print("-----------------------------------")
-num1 = eval(input("Informe um número: "))
-num2 = eval(input("Informe outro número: "))
-op = input("Informe a operação:\n 1 para Adição\n 2 para Subtração\n 3 para multiplicação\n 4 para divisão:\n ")
+num1 = eval(input("Digite o primeiro número : "))
+num2 = eval(input("Digite o segundo número : "))
+print("-----------------------------------")
+op = int(input("Informe a operação:\n 1 para Adição\n 2 para Subtração\n 3 para multiplicação\n 4 para divisão:\n "))
 print("-----------------------------------")
 
-if op == '1':
-    print("A soma de ", num1, "e", num2, " é: ", num1 + num2)
-elif op == '2':
-    print(num2, "subtraído de", num1, "resulta em: ", num1 - num2)
-elif op == '3':
-    print(num1, "multiplicado por", num2, "é igual a:", num1 * num2)
-elif op == '4':
-    print(num1, "dividido por", num2, "é igual a:", num1 / num2)
-else:
-    print("Operação inválida")
+def calculo(num1,num2,op):
+    if op == 1:
+        print("A soma de ", num1, "e", num2, " é: ", num1 + num2)
+    elif op == 2:
+        print(num2, "subtraído de", num1, "resulta em: ", num1 - num2)
+    elif op == 3:
+        print(num1, "multiplicado por", num2, "é igual a:", num1 * num2)
+    elif op == 4:
+        print(num1, "dividido por", num2, "é igual a:", num1 / num2)
+    else:
+        print("Operação inválida")
+
+calculo(num1, num2, op)
