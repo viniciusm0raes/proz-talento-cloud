5cab43f72cc1808b19cced97603c29aa5ee0a5ec
Vinícius de Moraes <92277345+viniciusm0raes@users.noreply.github.com>
2023-08-02 20:41:57 -0300

    Idade
    Verificação de Idade com verificação de erros (Try / Except).

---
+++ idade_Try_Except.py
@@ -0,0 +1,16 @@
+nome = str(input("Digite seu nome completo: "))
+anoCorreto = False
+while(anoCorreto == False):	
+	print("Digite seu ano de nascimento: ")
+	try:	
+		nasc = int(input())
+		idade = (2022-nasc)
+		if ((nasc >= 1922) and (nasc < 2022)):
+			anoCorreto = True
+			print("PARABÉNS!", nome, "\nVc completou",idade, "ano(s) de idade.")			
+	
+		else:
+			print("Revise o seu ano de nascimento.\nA informação digitada é invalida.")
+		
+	except:
+		print("A informação digitada é invalida.\nO ano precisa ser informado como um número INTEIRO.")
