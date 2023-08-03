91a16ca2215e9c076063baa5fc5e2f431f7ce41e
Vinícius de Moraes <92277345+viniciusm0raes@users.noreply.github.com>
2023-08-03 03:26:53 -0300

    Update idade_Try_Except.py
    Simplificando o código.

--- idade_Try_Except.py
+++ idade_Try_Except.py
@@ -1,16 +1,21 @@
 nome = str(input("Digite seu nome completo: "))
-anoCorreto = False
-while(anoCorreto == False):	
+while(True):	
 	print("Digite seu ano de nascimento: ")
 	try:	
 		nasc = int(input())
 		idade = (2022-nasc)
-		if ((nasc >= 1922) and (nasc < 2022)):
-			anoCorreto = True
-			print("PARABÉNS!", nome, "\nVc completou",idade, "ano(s) de idade.")			
+		if ((nasc >= 1922) and (nasc < 2022)):	
+			print("-"*35)		
+			print(nome + ",","PARABÉNS!", "\nVocê completou",idade, "ano(s) de idade.")			
+			print("-"*35)		
+			break
 	
 		else:
+			print("-"*53)		
 			print("Revise o seu ano de nascimento.\nDados válidos apenas no intervalo entre 1922 e 2021.")
+			print("-"*53)		
 		
 	except:
+		print("-"*53)		
 		print("A informação digitada é invalida.\nO ano precisa ser informado como um número INTEIRO.")
+		print("-"*53)		
