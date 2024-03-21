94ffc0430f52e1babff162216f40309c08df1923
viniciusm0raes <vinicius.moraes81@gmail.com>
2024-03-21 20:42:51 -0300

    update

---
+++ curso_extensao_frontend/modulo-1/api/fetch_api/exemplo-post/script.js
@@ -0,0 +1,6 @@
+const nomeProduto = document.getElementById('nome-produto');
+const valorProduto = document.getElementById('valor-produto');
+const descricaoProduto = document.getElementById('descricao-produto');
+const btnEnviar = document.getElementById('btn-enviar');
+const feedbackUsuario = document.getElementById('feedback-usuario')
+const produtosCadastrados = document.getElementById('produtos-cadastrados')
