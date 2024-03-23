e690dbe479ee7cc4bb37377fb3cde1a2f9cc79f7
viniciusm0raes <vinicius.moraes81@gmail.com>
2024-03-23 19:38:44 -0300

    update

---
+++ curso_extensao_frontend/modulo-1/api/fetch_api/exemplo-post/assets/scripts/script.js
@@ -0,0 +1,18 @@
+// 1. Captura de elementos
+const nomeProduto = document.getElementById('nome-produto');
+const valorProduto = document.getElementById('valor-produto');
+const descricaoProduto = document.getElementById('descricao-produto');
+const btnEnviar = document.getElementById('btn-enviar');
+const feedbackUsuario = document.getElementById('feedback-usuario');
+const produtosCadastrados = document.getElementById('produtos-cadastrados');
+const produtosCadastrados = document.getElementById('cadastrados');
+
+// 2. Funções
+
+//Utilizando o prepend para demonstrar os produtos cadastrados em ordem decrescente
+
+	postsContainer.prepend(post)
+
+//Limpar o formulário
+
+// 3. Eventos
